b09240bcae4b48eb315280ca38c5f92717a359f6
Vincent Bailly <insaslb@gmail.com>
2016-07-24 12:28:12 +0200

    Extract test for no question 1 min after an answer

--- Test/Test.cs
+++ Test/Test.cs
@@ -24,11 +24,10 @@ namespace Test
 		[Test()]
 		public void We_can_ask_twice_for_a_question()
 		{
-
 			var time = DateTime.Now;
-
-			Register_Question();
+			RegisterQuestion();
 			AskForNextQuestion(time);
+
 			var response = AskForNextQuestion(time);
 
 			Assert.AreEqual("The next question is:\n\"What is red?\"\n", response);
@@ -38,14 +37,13 @@ namespace Test
 		[Test()]
 		public void We_cannot_answer_when_there_is_no_question()
 		{
-
 			var time = DateTime.Now;
-
-			Register_Question();
+			RegisterQuestion();
 			AnswerCorrectly(time);
 			AskForNextQuestion(time);
 
 			var response = AnswerCorrectly(time);
+
 			Assert.AreEqual("You cannot answer because there is no question pending\n", response);
 
 		}
@@ -55,18 +53,12 @@ namespace Test
 		{
 			var time = DateTime.Now;
 
-			Register_Question();
+			RegisterQuestion();
 			AnswerCorrectly(time);
 			AskForNextQuestion(time);
-
-			time += TimeSpan.FromMinutes(1); // +1
+			time += TimeSpan.FromMinutes(3); // +3
 
 			var response = AskForNextQuestion(time);
-			Assert.AreEqual("There is no next question\n", response);
-
-			time += TimeSpan.FromMinutes(2); // +3
-
-			response = AskForNextQuestion(time);
 			Assert.AreEqual("The next question is:\n\"What is red?\"\n", response);
 
 			response = MainClass.TestableMain(new[] { "--answer", "an animal" }, time);
@@ -88,7 +80,7 @@ namespace Test
 		public void Wrong_answers_dont_pass()
 		{
 			var time = DateTime.Now;
-			Register_Question();
+			RegisterQuestion();
 
 			var response = MainClass.TestableMain(new[] { "--answer", "an animal" }, time);
 
@@ -99,7 +91,7 @@ namespace Test
 		public void An_wrong_answer_is_not_treated_if_no_question_is_pending()
 		{
 			var time = DateTime.Now;
-			Register_Question();
+			RegisterQuestion();
 			AnswerCorrectly(time);
 
 			var response = MainClass.TestableMain(new[] { "--answer", "an animal" }, time);
@@ -110,7 +102,7 @@ namespace Test
 		[Test()]
 		public void Register_a_new_entry()
 		{
-			var response = Register_Question();
+			var response = RegisterQuestion();
 
 			Assert.AreEqual("New entry registered\n", response);
 		}
@@ -118,7 +110,7 @@ namespace Test
 		[Test()]
 		public void A_question_is_available_straight_after_being_registered()
 		{
-			Register_Question();
+			RegisterQuestion();
 
 			var response = AskForNextQuestion(DateTime.Now);
 
@@ -128,7 +120,7 @@ namespace Test
 		[Test()]
 		public void Giving_a_correct_answer_for_the_first_time()
 		{
-			Register_Question();
+			RegisterQuestion();
 
 			var response = AnswerCorrectly(DateTime.Now);
 
@@ -138,12 +130,26 @@ namespace Test
 		[Test()]
 		public void There_is_no_question_just_after_having_answered_it()
 		{
-			Register_Question();
+			RegisterQuestion();
 			AnswerCorrectly(DateTime.Now);
 
 			var response = AskForNextQuestion(DateTime.Now);
 
 			Assert.AreEqual("There is no next question\n", response);
+		}
+
+		[Test()]
+		public void There_is_still_no_question_1min_after_having_answered_it()
+		{
+			var time = DateTime.Now;
+			RegisterQuestion();
+			AnswerCorrectly(time);
+			time += TimeSpan.FromMinutes(1);
+
+			var response = AskForNextQuestion(time);
+
+			Assert.AreEqual("There is no next question\n", response);
+
 		}
 
 		[Test ()]
@@ -152,17 +158,12 @@ namespace Test
 
 			var time = DateTime.Now;
 
-			Register_Question();
+			RegisterQuestion();
 			AnswerCorrectly(time);
 
-			time += TimeSpan.FromMinutes(1); // +1
+			time += TimeSpan.FromMinutes(3); // +3
 
 			var response = AskForNextQuestion(time);
-			Assert.AreEqual("There is no next question\n", response);
-
-			time += TimeSpan.FromMinutes(2); // +3
-
-			response = AskForNextQuestion(time);
 			Assert.AreEqual("The next question is:\n\"What is red?\"\n", response);
 
 			response = MainClass.TestableMain(new[] { "--answer", "a color" }, time);
@@ -187,7 +188,7 @@ namespace Test
 			return MainClass.TestableMain(new[] { "--next" }, time);
 		}
 
-		static string Register_Question()
+		static string RegisterQuestion()
 		{
 			return MainClass.TestableMain(new[] { "-q", "What is red?", "-a", "a color" }, DateTime.Now);
 		}
