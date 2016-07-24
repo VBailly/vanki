452c49e7b6f43eb3d42fd8bbefcc70eaa6da4f33
Vincent Bailly <insaslb@gmail.com>
2016-07-24 16:30:36 +0200

    Add more unit test for the second question

--- Test/TestWithTwoQuestions.cs
+++ Test/TestWithTwoQuestions.cs
@@ -8,7 +8,10 @@ namespace Test
 	public class TestWithTwoQuestion
 	{
 		static readonly string Question1 = "What is the color of a red fish?";
+		static readonly string Question2 = "What is the color of a blue fish?";
 		static readonly string NextQuestion1Message = string.Format(ConsoleOutputs.NextQuestionMessage, Question1);
+		static readonly string NextQuestion2Message = string.Format(ConsoleOutputs.NextQuestionMessage, Question2);
+
 
 		[SetUp]
 		public void SetUp()
@@ -22,8 +25,87 @@ namespace Test
 			var time = DateTime.Now;
 			Commands.RegisterQuestion(Question1, "red", time);
 			time += TimeSpan.FromSeconds(2);
-			Commands.RegisterQuestion("What is the color of a blue fish?", "blue", time);
+			Commands.RegisterQuestion(Question2, "blue", time);
 			time += TimeSpan.FromSeconds(2);
+			var response = Commands.AskForNextQuestion(time);
+
+			Assert.AreEqual(NextQuestion1Message, response);
+		}
+
+		[Test]
+		public void We_can_answer_the_first_question()
+		{
+			var time = DateTime.Now;
+			Commands.RegisterQuestion(Question1, "red", time);
+			time += TimeSpan.FromSeconds(2);
+			Commands.RegisterQuestion(Question2, "blue", time);
+			time += TimeSpan.FromSeconds(2);
+			var response = Commands.Answer(time, "red");
+
+			Assert.AreEqual(ConsoleOutputs.CorrectAnswerMessage, response);
+		}
+
+		[Test]
+		public void The_second_question_is_presented_after_we_answer_the_first_one()
+		{
+			var time = DateTime.Now;
+			Commands.RegisterQuestion(Question1, "red", time);
+			time += TimeSpan.FromSeconds(2);
+			Commands.RegisterQuestion(Question2, "blue", time);
+			time += TimeSpan.FromSeconds(2);
+			Commands.Answer(time, "red");
+			time += TimeSpan.FromSeconds(2);
+
+			var response = Commands.AskForNextQuestion(time);
+
+			Assert.AreEqual(NextQuestion2Message, response);
+		}
+
+		[Test]
+		public void The_second_question_can_be_properly_answered()
+		{
+			var time = DateTime.Now;
+			Commands.RegisterQuestion(Question1, "red", time);
+			time += TimeSpan.FromSeconds(2);
+			Commands.RegisterQuestion(Question2, "blue", time);
+			time += TimeSpan.FromSeconds(2);
+			Commands.Answer(time, "red");
+			time += TimeSpan.FromSeconds(2);
+			var response = Commands.Answer(time, "blue");
+
+			Assert.AreEqual(ConsoleOutputs.CorrectAnswerMessage, response);
+		}
+
+		[Test]
+		public void No_questions_available_after_the_two_answers()
+		{
+			var time = DateTime.Now;
+			Commands.RegisterQuestion(Question1, "red", time);
+			time += TimeSpan.FromSeconds(2);
+			Commands.RegisterQuestion(Question2, "blue", time);
+			time += TimeSpan.FromSeconds(2);
+			Commands.Answer(time, "red");
+			time += TimeSpan.FromSeconds(2);
+			Commands.Answer(time, "blue");
+
+			var response = Commands.AskForNextQuestion(time);
+
+			Assert.AreEqual(ConsoleOutputs.NoNextQuestionMessage, response);
+		}
+
+		[Test]
+		public void Question_available_again_after_the_two_answers_and_3_min()
+		{
+			var time = DateTime.Now;
+			Commands.RegisterQuestion(Question1, "red", time);
+			time += TimeSpan.FromSeconds(2);
+			Commands.RegisterQuestion(Question2, "blue", time);
+			time += TimeSpan.FromSeconds(2);
+			Commands.Answer(time, "red");
+			time += TimeSpan.FromSeconds(2);
+			Commands.Answer(time, "blue");
+			time += TimeSpan.FromMinutes(3);
+
 			var response = Commands.AskForNextQuestion(time);
 
 			Assert.AreEqual(NextQuestion1Message, response);
