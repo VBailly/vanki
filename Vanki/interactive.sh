72e9fe6f677c14646851379c8faef9321b1a6c3b
Vincent Bailly <insaslb@gmail.com>
2016-07-25 14:05:19 +0200

    completed the interactive script

--- Vanki/interactive.sh
+++ Vanki/interactive.sh
@@ -1,8 +1,35 @@
 #!/bin/bash
 
-while true
-do
-	mono Vanki.exe -n
-	read answer
-	mono Vanki.exe -a "$answer"
-done
+
+function play {
+
+	while true
+	do
+		mono Vanki.exe -n
+		read answer
+		mono Vanki.exe -a "$answer"
+		echo "-------------------------------------"
+	done
+}
+
+function add {
+	while true
+	do
+		echo "Type the question"
+		read question
+		echo "Type the answer"
+		read answer
+		mono Vanki.exe -q "$question" -a "$answer"
+		echo "-------------------------------------"
+	done
+}
+
+if [ ${1} == "play" ]
+then
+	play
+elif [ ${1} == "add" ]
+then
+	add
+else
+	echo "You much type either play or add"
+fi
