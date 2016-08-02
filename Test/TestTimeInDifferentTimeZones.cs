5f1d5fbca98cc695fc8c037c34ce9725e28f7978
Vincent Bailly <insaslb@gmail.com>
2016-08-02 19:35:34 -0400

    make test for changement of time zone

--- Test/TestTimeInDifferentTimeZones.cs
+++ Test/TestTimeInDifferentTimeZones.cs
@@ -15,6 +15,59 @@ namespace Test
             Clock.HoursDifferenceFromGlobal = null;
         }
 
+        [TearDown]
+        public void TearDown()
+        {
+            Clock.LocalTimeGetter = null;
+            Clock.HoursDifferenceFromGlobal = null;
+        }
+
+        [Test]
+        public void Traveling_to_west_does_not_disturb_progress()
+        {
+            var time = DateTime.Now;
+
+            Clock.LocalTimeGetter = () => time;
+            Clock.HoursDifferenceFromGlobal = () => 0;
+
+            Commands.RegisterQuestion("What is red?", "a color");
+            Commands.Answer("a color");
+
+            time += TimeSpan.FromMinutes(5);
+            Clock.LocalTimeGetter = () => time ;
+            Commands.Answer("a color");
+
+            time += TimeSpan.FromHours(5);
+            Clock.HoursDifferenceFromGlobal = () => 5;
+
+            var response = Commands.AskForNextQuestion();
+
+            Assert.IsTrue(response.Contains(ConsoleOutputs.NoNextQuestionMessage));
+        }
+
+        [Test]
+        public void Traveling_to_east_does_not_disturb_progress()
+        {
+            var time = DateTime.Now;
+
+            Clock.LocalTimeGetter = () => time;
+            Clock.HoursDifferenceFromGlobal = () => 0;
+
+            Commands.RegisterQuestion("What is red?", "a color");
+            Commands.Answer("a color");
+
+            time += TimeSpan.FromMinutes(5);
+            Clock.LocalTimeGetter = () => time;
+            Commands.Answer("a color");
+
+            time -= TimeSpan.FromHours(5);
+            Clock.HoursDifferenceFromGlobal = () => -5;
+
+            var response = Commands.AskForNextQuestion();
+
+            Assert.IsTrue(response.Contains(ConsoleOutputs.NoNextQuestionMessage));
+        }
+
 
     }
 }
