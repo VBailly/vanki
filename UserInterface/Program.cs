ab6eea5cf283f9ed9b22d1d6d73f317e801b76b0
Vincent Bailly <insaslb@gmail.com>
2016-09-21 21:07:02 +0200

    introduce project for the interactive user interface (but still command line because we love it)

---
+++ UserInterface/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace UserInterface
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Vanki.MainClass.Main(args);
+        }
+    }
+}
