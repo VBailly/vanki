4e3049552493bdfb31e69a09bb75852993a433fc
Vincent Bailly <insaslb@gmail.com>
2016-11-25 22:41:21 +0100

    add files I missed previously

---
+++ DeckAPI/IDeck.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DeckAPI
+{
+    public interface IDeck
+    {
+        DateTime GetNextCardDueTime();
+        string GetNextQuestion();
+        bool NextCardNeedsAClue();
+        string GetClue();
+        DeckState GetState(DateTime now);
+
+        DeckOperationResult AddNewCard(IEnumerable<string> questions, IEnumerable<string> answers, bool caseSensitive, DateTime now);
+        DeckOperationResult ProcessAnswer(string answer, DateTime now);
+        DeckOperationResult RevertLastWrongAnswer(bool add, DateTime now);
+    
+    }
+
+}
